a445878a0fbae0697b32440bc15d5b801d1a66d9
tahaimage8 <tahaimage8@gmail.com>
2026-05-25 17:38:56 +0600

    Complete the first attempt and also get from action anything from server

---
+++ index.js
@@ -0,0 +1,61 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+
+
+
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("sportNestDB")
+
+    const facilities  = db.collection("facilities");
+
+
+    app.post('/facilities', async (req, res)=> {
+      const newFacility = req.body;
+      const result = await facilities.insertOne(newFacility);
+      res.send(result);
+    })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+  res.send('Server Is Running Fine');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
