a3d3909b196edc3859b27c60c34f69a8b6860a3a
tahaimage8 <tahaimage8@gmail.com>
2026-05-26 03:50:56 +0600

    Update the delete method

--- index.js
+++ index.js
@@ -60,6 +60,12 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/facilities/:id', async (req, res) => {
+      const { id } = req.params;
+      const result = await facilities.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
